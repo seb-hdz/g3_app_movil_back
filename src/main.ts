4fbd6f6fd1ccac90c88c6a70fe7d1f2925608c3f
Sebastian Hidalgo <sebastian.hidalgo@unmsm.edu.pe>
2024-07-12 15:41:41 -0500

    feat(global): add api docs (swagger)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
@@ -6,6 +7,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('G3 App Backend (API)')
+    .setDescription('Endpoints and services')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
